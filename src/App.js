f058eb0752193302b54500e6a33605a22649d0d2
Anne Kala <annekala1127@gmail.com>
2022-05-25 17:24:08 -0700

    modified header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import logo from "./logo.png";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        {" "}
+        <span>
+          {" "}
+          <img src={logo} className="App-logo" alt="logo" />{" "}
+        </span>{" "}
+        Dictionary
+      </header>
     </div>
   );
 }
