c02471fbd0ebe9b055dd788a7c3c2d70208da487
Anne Kala <annekala1127@gmail.com>
2022-05-28 12:13:48 -0700

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,18 +9,15 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              <strong>Definition:</strong>
-              {definition.definition} <br />{" "}
-              <em>
-                {" "}
-                <strong>Example</strong>
-                {definition.example}
-              </em>
-              <Synonyms synonyms={definition.synonyms} />
+              <strong>Definition: </strong>
+              {definition.definition} <br /> <strong>Example: </strong>{" "}
+              <em> {definition.example}</em>
+              <em> {definition.synonyms}</em>
             </p>
           </div>
         );
       })}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
