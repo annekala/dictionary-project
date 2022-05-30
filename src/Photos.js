551a305d33465811a1d5a5b8ed5e81e3c924ef58
Anne Kala <annekala1127@gmail.com>
2022-05-30 00:54:20 -0700

    modified photos

--- src/Photos.js
+++ src/Photos.js
@@ -10,11 +10,7 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a
-                  href={photo.src.original}
-                  rel="noreferrer"
-                  target="_blank"
-                ></a>
+                <a href={photo.src.original} rel="noreferrer"></a>
                 <img src={photo.src.landscape} className="img-fluid" alt="" />
               </div>
             );
